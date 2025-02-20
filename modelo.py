91877fd6cd71d394550404ac75b50cf731584575
Kevin Mateo Gomez <gomezdiezkevinmateo@gmail.com>
2025-02-19 19:49:40 -0500

    movimiento

--- modelo.py
+++
@@ -1,4 +0,0 @@
-def calculate_pension(ibl: float, pension_porcentage: float, smmlv: float):
-    pension_porcentage = pension_porcentage/100
-    tasa_remplazo = pension_porcentage-((0.5/100)*(ibl/smmlv))
-    return ibl*tasa_remplazo
