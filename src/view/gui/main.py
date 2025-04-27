ca387e2115773a509f79b2b6b6f9e0cd0441b3a3
Mayday <maydaycorrea@gmail.com>
2025-04-27 14:07:36 -0500

    Resolver conflictos después de pull

--- src/view/gui/main.py
+++ src/view/gui/main.py
@@ -23,8 +23,6 @@ from model.Error_Pension import (
 kv_path = os.path.join(os.path.dirname(__file__), "pension_gui.kv")
 Builder.load_file(kv_path)
 
-
-
 # Clase que define el layout principal de la GUI
 class PensionLayout(BoxLayout):
     # Valor por defecto del SMMLV (Salario Mínimo Mensual Legal Vigente)
